8ce174a58884274c9ece85bb6b30fbcffdc5c9f5
Srigopinath-A <sgopinath1930@gmail.com>
2025-04-29 07:08:00 +0000

    Docker image converstion

--- Awx-automation/Awx-automation/src/main/java/com/example/Awx_automation/Entity/Project.java
+++ Awx-automation/Awx-automation/src/main/java/com/example/Awx_automation/Entity/Project.java
@@ -36,4 +36,120 @@ public class Project {
     private boolean allowOverride = false;
     private Long defaultEnvironment;
     private Long signatureValidationCredential;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getScmType() {
+		return scmType;
+	}
+	public void setScmType(String scmType) {
+		this.scmType = scmType;
+	}
+	public String getLocalPath() {
+		return localPath;
+	}
+	public void setLocalPath(String localPath) {
+		this.localPath = localPath;
+	}
+	public String getScmUrl() {
+		return scmUrl;
+	}
+	public void setScmUrl(String scmUrl) {
+		this.scmUrl = scmUrl;
+	}
+	public String getScmBranch() {
+		return scmBranch;
+	}
+	public void setScmBranch(String scmBranch) {
+		this.scmBranch = scmBranch;
+	}
+	public String getScmRefspec() {
+		return scmRefspec;
+	}
+	public void setScmRefspec(String scmRefspec) {
+		this.scmRefspec = scmRefspec;
+	}
+	public boolean isScmClean() {
+		return scmClean;
+	}
+	public void setScmClean(boolean scmClean) {
+		this.scmClean = scmClean;
+	}
+	public boolean isScmTrackSubmodules() {
+		return scmTrackSubmodules;
+	}
+	public void setScmTrackSubmodules(boolean scmTrackSubmodules) {
+		this.scmTrackSubmodules = scmTrackSubmodules;
+	}
+	public boolean isScmDeleteOnUpdate() {
+		return scmDeleteOnUpdate;
+	}
+	public void setScmDeleteOnUpdate(boolean scmDeleteOnUpdate) {
+		this.scmDeleteOnUpdate = scmDeleteOnUpdate;
+	}
+	public Long getCredential() {
+		return credential;
+	}
+	public void setCredential(Long credential) {
+		this.credential = credential;
+	}
+	public int getTimeout() {
+		return timeout;
+	}
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
+	public Long getOrganization() {
+		return organization;
+	}
+	public void setOrganization(Long organization) {
+		this.organization = organization;
+	}
+	public boolean isScmUpdateOnLaunch() {
+		return scmUpdateOnLaunch;
+	}
+	public void setScmUpdateOnLaunch(boolean scmUpdateOnLaunch) {
+		this.scmUpdateOnLaunch = scmUpdateOnLaunch;
+	}
+	public int getScmUpdateCacheTimeout() {
+		return scmUpdateCacheTimeout;
+	}
+	public void setScmUpdateCacheTimeout(int scmUpdateCacheTimeout) {
+		this.scmUpdateCacheTimeout = scmUpdateCacheTimeout;
+	}
+	public boolean isAllowOverride() {
+		return allowOverride;
+	}
+	public void setAllowOverride(boolean allowOverride) {
+		this.allowOverride = allowOverride;
+	}
+	public Long getDefaultEnvironment() {
+		return defaultEnvironment;
+	}
+	public void setDefaultEnvironment(Long defaultEnvironment) {
+		this.defaultEnvironment = defaultEnvironment;
+	}
+	public Long getSignatureValidationCredential() {
+		return signatureValidationCredential;
+	}
+	public void setSignatureValidationCredential(Long signatureValidationCredential) {
+		this.signatureValidationCredential = signatureValidationCredential;
+	}
+    
+    
 }
